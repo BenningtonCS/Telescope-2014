d00e6584f3d5817b461e61bfe77750afd456187c
edaniszewski <edaniszewski@gmail.com>
2014-03-06 00:12:10 -0500

    now getIP.sh will put the gotten IP address into the srt.cat file for you, Presto alakazam

--- srtnver3/getIP.sh
+++ srtnver3/getIP.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-curl -S http://www.ipinfodb.com/my_ip_location.php | sed 's/^[ \t]*//' | grep '<li>City\|<li>IP address\|<li>Country\|<li>Latitude\|<li>Longitude' >> tmpfile.txt 
-sed -e 's/<[^>]*>//g' tmpfile.txt >> ipdata.txt
-rm tmpfile.txt
+# From a IP-to-Geolocation website, parse out the data so we are left with the location, latitude, and longitude
+curl -S http://www.ipinfodb.com/my_ip_location.php | sed 's/^[ \t]*//' | grep '<li>Latitude\|<li>Longitude\|<li>City' | sed -e 's/<[^>]*>//g;s/^.*://' >> ipdata.txt 
+# Get the Location, Lat, and Long stored in var1, var2, and var3, respectively
+eval $(awk '{print "var"NR"="$1}' ipdata.txt)
+# Append the geolocation information with the keyword to the srt.cat file
+echo "STATION $var2 $var3 $var1" | tr -d '\r' >> srt.cat
